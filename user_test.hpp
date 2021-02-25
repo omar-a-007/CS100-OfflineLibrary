99a69507e362c2127077b87e00254f58b6225a09
XirongXie <xirongxie@yahoo.com>
2021-02-25 08:19:52 -0800

    Created tests for user class

---
+++ user_test.hpp
@@ -0,0 +1,43 @@
+#ifndef __USER_TEST_HPP__
+#define __USER_TEST_HPP__
+
+#include "gtest/gtest.h"
+#include "headers/User.h"
+
+//Test getPrivelageLevel()
+TEST(pLevel, Default){
+    EXPECT_EQ(getPrivelageLevel(), 0);
+}
+
+TEST(pLevel, Positive){
+    setPrivelageLevel(1);
+    EXPECT_EQ(getPrivelageLevel(), 1);
+}
+
+TEST(pLevel, Negative){
+    setPrivelageLevel(-1);
+    EXPECT_EQ(getPrivelageLevel(), -1);
+}
+
+TEST(pLevel, LargeNumber){
+    setPrivelageLevel(10000);
+    EXPECT_EQ(getPrivelageLevel(), 10000);
+}
+
+//Test getUsername()
+TEST(gName, Empty){
+    User(, password);
+    EXPECT_EQ(getUsername(), NULL);
+}
+
+TEST(gName, Lowercase){
+    User(email, password);
+    EXPECT_EQ(getUsername(), email);
+}
+
+TEST(gName, Uppercase){
+    User(EMAIL, password);
+    EXPECT_EQ(getUsername(),EMAIL);
+}
+
+#endif
