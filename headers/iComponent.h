db56696fc3067b2b0ceeacae6bbd303e9f178abe
oalku001 <oalku001@ucr.edu>
2021-02-27 19:24:58 -0800

    Built out the composite classes (iComponent, Media(+Book, DVD, AudioBook), Category).

--- headers/iComponent.h
+++ headers/iComponent.h
@@ -3,76 +3,92 @@
 
 #include <string>
 #include <vector>
+#include <iostream>
+#include <unordered_map>
 
 class iComponent{
     protected:        std::string title;
     public:
-        iComponent(std::string title)
-            :title(title) {};
+        iComponent(const std::string& title)
+            : title(title) {};
         virtual ~iComponent() {};
-        virtual void display() = 0;
-};
-
-class Category : public iComponent{
-    private:
-        int CID;
-        std::vector<iComponent *> children;
-    public:
-        Category(std::string Title)
-            :iComponent(Title) {};
-        ~Category() override;
-        void display() override
-            {for (auto c : children) c->display();}
-        int itemCount() const
-            {return children.size();};
-        void add(iComponent *);
-        void remove(iComponent *);
-        iComponent* getChildren();  // <-- is this even needed with the children vector ?
+        virtual void display(const std::string& prepend = "", std::ostream& stream = std::cout) = 0;
+        const std::string& getTitle() const {return this->title;};
 };
 
 class Media : public iComponent{
     protected:
-        int MID;
         std::string Author;
-        int quantityAvailable;
         int contentLength;  //pages, minutes, etc
         double cost;
+        int MID;
+        int CID;
+        int quantityAvailable;
     public:
-        Media(std::string Title, std::string Author, int contentLength, double cost)
-            : iComponent(Title), Author(Author), contentLength(contentLength), cost(cost) {};
+        Media(const std::string& Title, const std::string& Author, int contentLength, double cost, int MID = 0, int CID = 0, int quantityAvailable = 0)
+            : iComponent(Title), Author(Author), contentLength(contentLength), cost(cost), MID(MID), CID(CID), quantityAvailable(quantityAvailable) {};
         virtual ~Media() {};
-        virtual void display() = 0;
-        std::string getTitle() const                {return this->title;};
+
+        virtual void display(const std::string& prepend = "", std::ostream& stream = std::cout) = 0;
+
+        const std::string& getTitle() const         {return this->title;};
         void setTitle(const std::string& Title)     {this->title = Title;};
+
         void setQuantityAvailable(int quantity)     {this->quantityAvailable = quantity;};
-        int getQuantityAvailable() const            {return this->quantityAvailable;};
+        const int getQuantityAvailable() const      {return this->quantityAvailable;};
+
         void setCost(double cost)                   {this->cost = cost;};
 };
 
 class DVD : public Media {
     public:
-        DVD(std::string Title, std::string Author, int contentLength, double Cost = 19.99)
-            : Media(Title, Author, contentLength, Cost) {};
+        DVD(const std::string& Title, const std::string& Author, int contentLength, double Cost = 19.99, int MID = 0, int CID = 0, int quantityAvailable = 0)
+            : Media(Title, Author, contentLength, Cost, MID, CID, quantityAvailable) {};
         ~DVD();
-        void display() override;
+        void display(const std::string& prepend = "", std::ostream& stream = std::cout) override;
 };
 
 class Book : public Media {
     private:
         std::string ISBN;
     public:
-        Book(std::string Title, std::string Author, std::string ISBN, int contentLength, double Cost = 9.99)
-            :Media(Title, Author, contentLength, Cost), ISBN(ISBN) {};
+        Book(const std::string& Title, const std::string& Author, const std::string& ISBN, int contentLength, double Cost = 9.99, int MID = 0, int CID = 0, int quantityAvailable = 0)
+            :Media(Title, Author, contentLength, Cost, MID, CID, quantityAvailable), ISBN(ISBN) {};
         ~Book();
-        void display() override;
+        void display(const std::string& prepend = "", std::ostream& stream = std::cout) override;
 };
 
 class AudioBook : public Media {
     public:
-        AudioBook(std::string Title, std::string Author, int contentLength, double Cost = 9.99)
-            :Media(Title, Author, contentLength, Cost) {};
+        AudioBook(const std::string& Title, const std::string& Author, int contentLength, double Cost = 19.99, int MID = 0, int CID = 0, int quantityAvailable = 0)
+            : Media(Title, Author, contentLength, Cost, MID, CID, quantityAvailable) {};
         ~AudioBook();
-        void display() override;
+        void display(const std::string& prepend = "", std::ostream& stream = std::cout) override;
+};
+
+
+
+class Category : public iComponent{
+    private:
+        int CID;
+        int PID;
+        std::vector<iComponent *> children;
+        //std::unordered_map<std::string, iComponent*> children2;
+    public:
+        Category(const std::string& title, int CID = 0, int PID = 0)
+            : iComponent(title) {};
+        ~Category() override;
+        void display(const std::string& prepend = "", std::ostream& stream = std::cout) override;
+
+        const int itemCount() const;
+        void add(iComponent* component);
+        void remove(iComponent* component);
+
+        Media* findMedia(std::string title);
+        
+
+
+        std::vector<iComponent*> getChildren();  // <-- is this even needed with the children vector ?
 };
 
 #endif
--- src/iComponent.cpp
+++ src/iComponent.cpp
@@ -1,34 +1,115 @@
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+
 #include "../headers/iComponent.h"
 
-Category::~Category()
-{
-    
-}
-
-void Category::add(iComponent* cmp)
-{
-
-}
-
-void Category::remove(iComponent* cmp)
-{
-    
-}
-
 Book::~Book()  {}
-void Book::display()
+void Book::display(const std::string& prepend, std::ostream& stream)
 {
-
+    stream << prepend << "[Book] " << this->title << std::endl;
+        stream << prepend << "\t" << "Author: " << this->Author << std::endl;
+        stream << prepend << "\t" << std::setw(28) << std::left << "Pages: " + std::to_string(contentLength);
+                        stream << "ISBN: " << this->ISBN << std::endl;
+        stream << prepend << "\t" << std::setw(28) << std::left << "Quantity Available: " + std::to_string(quantityAvailable);
+                        stream << "Replacement Cost: " << this->cost << std::endl;
 }
 
 AudioBook::~AudioBook()  {}
-void AudioBook::display()
+void AudioBook::display(const std::string& prepend, std::ostream& stream)
 {
-
+    stream << prepend << "[AudioBook] " << this->title << std::endl;
+        stream << prepend << "\t" << "Narrator: " << this->Author << std::endl;
+        stream << prepend << "\t" << "Minutes: " << this->contentLength << std::endl;
+        stream << prepend << "\t" << std::setw(28) << std::left << "Quantity Available: " + std::to_string(quantityAvailable);
+                        stream << "Replacement Cost: " << this->cost << std::endl;
 }
 
 DVD::~DVD() {}
-void DVD::display()
+void DVD::display(const std::string& prepend, std::ostream& stream)
+{
+    stream << prepend << "[DVD] " << this->title << std::endl;
+        stream << prepend << "\t" << "Producer: " << this->Author << std::endl;
+        stream << prepend << "\t" << "Minutes: " << this->contentLength << std::endl;
+        stream << prepend << "\t" << std::setw(28) << std::left << "Quantity Available: " + std::to_string(quantityAvailable);
+                        stream << "Replacement Cost: " << this->cost << std::endl;
+}
+
+
+
+Category::~Category()
+{
+    for (auto& c : children)
+    {
+        std::cout << "Destructor called for: " << c->getTitle() << std::endl;
+        delete c;
+    } 
+}
+
+const int Category::itemCount() const
+{
+    int i = 0;
+    for (const auto& c : children)
+    {
+        if (Category* cat = dynamic_cast<Category*>(c))
+            i += cat->itemCount();
+        else if (Media* m = dynamic_cast<Media*>(c))
+            ++i;
+    }
+    return i;
+}
+
+/* Category.add
+    Currently not using a hashmap because:
+    Using an ordered_map doesnt allow duplicate keys. 
+        Using title as the key means Category and Media can't have the same title
+        Possible Solution 1: Two add functions for Categories and Media, value is "MID/CID ###"
+        Possible Solution 2: Boost::Multi_Index_Key
+    {children2.insert(std::make_pair(component->getTitle(), component));}
+*/
+void Category::add(iComponent* component)
+{
+    children.push_back(component);
+}
+
+void Category::remove(iComponent* cmp)
+{    
+    auto iter = std::find(children.begin(), children.end(), cmp);
+    if (iter != children.end()) {
+        children.erase(iter);
+        delete cmp;
+        return;
+    }
+    for (const auto& c : children)
+        if (Category* cat = dynamic_cast<Category*>(c))
+            cat->remove(cmp);    
+}
+
+Media* Category::findMedia(std::string title)
 {
 
+    for (const auto& c : children)
+    {
+        if (Category* cat = dynamic_cast<Category*>(c))
+        {
+            Media* temp = cat->findMedia(title);
+            if (temp != nullptr) return temp;
+        }
+        else if (Media* m = dynamic_cast<Media*>(c))
+            if (m->getTitle() == title) return m;
+    }
+    return nullptr;
+}
+
+std::vector<iComponent*> Category::getChildren()
+{
+    return children;
+}
+
+void Category::display(const std::string& prepend, std::ostream& stream)
+{   stream << prepend << std::endl;
+    stream << prepend << "Category: " << title << std::endl;
+    stream << prepend << "------------------------------------------------"  << std::endl;
+    for (const auto& c : children) c->display(prepend + "\t", stream);
+    //for (const auto& c : children2) c.second->display(prepend + "\t", stream);
 }
